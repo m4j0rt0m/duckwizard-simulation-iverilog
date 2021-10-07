a0d020e939267531c2b8831a84a707da54542fb0
m4j0rt0m <major.tom.spaceworm@gmail.com>
2021-10-07 19:29:05 +0200

    :memo: update print-rtl-srcs function

--- scripts/funct.mk
+++ scripts/funct.mk
@@ -1,6 +1,6 @@
 ### function define: veritedium AUTO directive ###
 define veritedium-command
-emacs --batch $(1) -f verilog-auto -f save-buffer;
+emacs --batch $(1) -f verilog-auto -f save-buffer >/dev/null 2>&1;
 endef
 
 ### function define: print sources ###
@@ -12,13 +12,18 @@ define print-srcs-command
 	do\
 		echo "  |-> $${vsrc}";\
 	done
-	@echo " [+] Verilog Headers Files: $(words $(VERILOG_HEADERS))";\
-	for vheader in $(VERILOG_HEADERS);\
+	@echo " [+] SystemVerilog Source Files: $(words $(SVERILOG_SRC))";\
+	for svsrc in $(SVERILOG_SRC);\
+	do\
+		echo "  |-> $${svsrc}";\
+	done
+	@echo " [+] Verilog Headers Files: $(words $(VERILOG_HEADERS) $(SVERILOG_HEADERS))";\
+	for vheader in $(VERILOG_HEADERS) $(SVERILOG_HEADERS);\
 	do\
 		echo "  |-> $${vheader}";\
 	done
 	@echo " [+] Packages Source Files: $(words $(PACKAGE_SRC))";\
-	for psrc in $(PACKAGES_SRC);\
+	for psrc in $(PACKAGE_SRC);\
 	do\
 		echo "  |-> $${psrc}";\
 	done
@@ -28,4 +33,24 @@ define print-srcs-command
 		echo "  |-> $${msrc}";\
 	done;\
 	echo -e "$(_reset_)"
+endef
+
+### function define: print filelist ###
+define print-filelist
+	@> $(1);\
+	for idir in $(INCLUDE_DIRS);\
+	do\
+		{ echo '+incdir+$${DESIGN_RTL_DIR}/'; echo "$${idir}" | grep -oP "^$(2)/\K.*";} | tr -d "\n"  >> $(1);\
+		echo "" >> $(1);\
+	done;\
+	for psrc in $(PACKAGE_SRC);\
+	do\
+		{ echo '$${DESIGN_RTL_DIR}/'; echo "$${psrc}" | grep -oP "^$(2)/\K.*";} | tr -d "\n" >> $(1);\
+		echo "" >> $(1);\
+	done;\
+	for vsrc in $(VERILOG_SRC);\
+	do\
+		{ echo '$${DESIGN_RTL_DIR}/'; echo "$${vsrc}" | grep -oP "^$(2)/\K.*";} | tr -d "\n" >> $(1);\
+		echo "" >> $(1);\
+	done
 endef
